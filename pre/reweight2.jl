e477f1af2774963bafe4e7d33b4b517ef2d076b3
Jonathan Zung <jonathanzung@gmail.com>
2017-03-29 14:41:04 -0700

    fix maxval in case of negative weights

--- pre/reweight2.jl
+++ pre/reweight2.jl
@@ -4,7 +4,7 @@ function relabel(labels,relabels)
 	return map(i->relabels[i], labels)
 end
 
-function gen_weights(labels; M=30, kernel_size=[100,100,20])
+function gen_weights(labels; M=30, kernel_size=[100,100,20], max_factor=20)
 	labels+=1
 	N=maximum(labels)
 	tmp = randn(N,M)
@@ -48,7 +48,7 @@ function gen_weights(labels; M=30, kernel_size=[100,100,20])
 		if labels[i]==1
 			weights[i]=0
 		else
-			weights[i] = 1 ./ max(0.05, weights[i])
+			weights[i] = 1f0 / min(1f0,max(1f0/max_factor, weights[i]))
 		end
 	end
 	return weights
@@ -57,6 +57,7 @@ end
 #We always output zyx, zero based indices
 function gen_samples(labels; patch_size=Int[318,318,48], kernel_size=map(x->round(Int,x/2),patch_size), N=100000, M=30, mask=1)
 	weights = gen_weights(labels, kernel_size = kernel_size, M=M) .* mask
+	Save.save("weights.h5",weights)
 	weights *= (N/sum(weights))
 
 	A=Vector{Int}[]
@@ -70,3 +71,13 @@ function gen_samples(labels; patch_size=Int[318,318,48], kernel_size=map(x->roun
 	println("$(length(A)) examples collected")
 	return flatten_samples(map(x->reverse(x)-1,A))
 end
+
+function stars(shape,samples)
+	samples = samples[end:-1:1,:]+1
+	tmp=zeros(UInt32,shape)
+	for i in 1:size(samples,2)
+		tmp[samples[1,i],samples[2,i],samples[3,i]]=1
+	end
+	return tmp
+end
+
